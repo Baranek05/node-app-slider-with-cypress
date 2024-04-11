a668790866dce98ee2f4666a1a224c01ef8052b2
Baranek05 <baranjakub0525@gmail.com>
2024-04-11 12:17:10 +0200

    modified:   cypress/e2e/slider.cy.js

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,43 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if each slide contains correct title and description', function () {
+    cy.visit('http://localhost:3000');
+
+    cy.get('.swiper-slide').each(($slide, index) => {
+      cy.wrap($slide).find('.slide-title').then(($title) => {
+        const titleText = $title.text().trim();
+        expect(titleText).to.not.be.empty;
+        switch (index) {
+          case 0:
+            expect(titleText).to.equal('Rome');
+            break;
+          case 1:
+            expect(titleText).to.equal('London');
+            break;
+          case 2:
+            expect(titleText).to.equal('Paris');
+            break;
+          default:
+        }
+      });
+
+      cy.wrap($slide).find('.slide-description').then(($description) => {
+        switch (index) {
+          case 0:
+            expect(descriptionText).to.equal('Italy');
+            break;
+          case 1:
+            expect(descriptionText).to.equal('United Kingdom');
+            break;
+          case 2:
+            expect(descriptionText).to.equal('United Kingdom');
+            break;
+          default:
+        }
+      });
+    });
+  });
+});
